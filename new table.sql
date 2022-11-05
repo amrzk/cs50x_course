f77e17fc5ab277a1c581f2a84843ab797a84d42f
itchy0head <112691044+itchy0head@users.noreply.github.com>
2022-11-05 19:09:18 +0000

    cleaning

--- new table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS purchase (
-    id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	symbol text NOT NULL,
-    price NUMERIC NOT NULL,
-    shares integer NOT NULL,
-	date text NOT NULL,
-	user_id integer NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users (id)
-);
