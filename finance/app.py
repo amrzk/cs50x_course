97629a354798bf09f60e75073b8177684054c425
Ahmed Marzouk <itchy0head@gmail.com>
2022-11-11 18:44:00 +0200

    heroku

--- finance/app.py
+++ finance/app.py
@@ -25,12 +25,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-# uri = os.getenv("DATABASE_URL")
-# if uri.startswith("postgress://"):
-#     uri = uri.replace("postgres://", "postgresql://")
-# db = SQL(uri)
-db = SQL("sqlite:///finance.db")
+Configure CS50 Library to use SQLite database
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgress://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
+# db = SQL("sqlite:///finance.db")
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
